9db133e0b9b3293b1c5332c2beeab7b6fa8d37e7
Nathaniel MacArthur-Warner <nathanielwarner77@gmail.com>
2020-12-21 11:45:04 -0800

    Merge changes from ludomotico on FG forum, add USGS server

--- creator.py
+++ creator.py
@@ -1,14 +1,36 @@
 import math
 import argparse
 import requests
+import logging
 import os
+import tempfile
+from PIL import Image, UnidentifiedImageError
+
+
+# Ortophoto servers
+# The url is a format string. 'tbounds(minlon,minlat,maxlon,maxlat)' is used for tile bounds and 'tsize(width,height)' for tile size, in pixels.
+URLS = {
+    # ArcGIS. suitable for the entire world, license forbids using these images offline
+    'ArcGIS': 'http://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/export?bbox={tbounds[0]},{tbounds[1]},{tbounds[2]},{tbounds[3]}&bboxSR=4326&size={tsize[0]},{tsize[1]}&imageSR=4326&format=png24&f=image',
+    # PNOA. only Spain. license CC-BY
+    # https://pnoa.ign.es/presentacion-y-objetivo
+    'PNOA': 'https://www.ign.es/wms-inspire/pnoa-ma?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&LAYERS=OI.OrthoimageCoverage&SRS=EPSG:4326&BBOX={tbounds[0]},{tbounds[1]},{tbounds[2]},{tbounds[3]}&WIDTH={tsize[0]}&HEIGHT={tsize[1]}&FORMAT=image/png',
+    # USGS. United States only. Public domain license, see:
+    # https://www.usgs.gov/faqs/what-are-terms-uselicensing-map-services-and-data-national-map?qt-news_science_products=0#qt-news_science_products
+    'USGS': 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/export?bbox={tbounds[0]},{tbounds[1]},{tbounds[2]},{tbounds[3]}&bboxSR=4326&size={tsize[0]},{tsize[1]}&imageSR=4326&format=png24&f=image',
+}
+
+# Tile height, in degrees. This is a constant for FG
+TILE_HEIGHT = 0.125
 
 
 def get_tile_width(lat):
+    """ Gets the width of a FG tile, in degrees. In FG, the width of a tile depends on the latitude """
     tile_table = [[0, 0.125], [22, 0.25], [62, 0.5], [76, 1], [83, 2], [86, 4], [89, 12]]
     for i in range(len(tile_table)):
         if tile_table[i][0] <= abs(lat) < tile_table[i + 1][0]:
             return float(tile_table[i][1])
+
 
 
 class Bucket(object):
@@ -19,7 +41,7 @@ class Bucket(object):
         self.y = y
 
     def __str__(self):
-        return '{lon: %d, lat: %d, x: %d, y: %d}' % (self.lon, self.lat, self.x, self.y)
+        return str(self.get_bounds())
 
     def get_index(self):
         return ((self.lon + 180) << 14) + ((self.lat + 90) << 6) + (self.y << 3) + self.x
@@ -66,18 +88,20 @@ class Bucket(object):
         if main_lat < 0:
             main_lat *= -1
 
-        return '%s%03d%s%02d/%s%03d%s%02d' % (hem, top_lon, pole, top_lat, hem, main_lon, pole, main_lat)
+        return os.path.join('%s%03d%s%02d' % (hem, top_lon, pole, top_lat), '%s%03d%s%02d' % (hem, main_lon, pole, main_lat))
 
     @classmethod
     def from_index(cls, index):
+        """ Factory method: create a Bucket from a FlightGear index """
         lon = (index >> 14) - 180
         lat = ((index - ((lon + 180) << 14)) >> 6) - 90
         y = (index - (((lon + 180) << 14) + ((lat + 90) << 6))) >> 3
         x = index - ((((lon + 180) << 14) + ((lat + 90) << 6)) + (y << 3))
-        return Bucket(lon, lat, x, y)
+        return cls(lon, lat, x, y)
 
     @classmethod
     def from_lon_lat(cls, d_lon, d_lat):
+        """ Factory method: create a Bucket from a lon, lat pair """
         lat = math.floor(d_lat)
         y = int((d_lat - lat) * 8)
         width = get_tile_width(lat)
@@ -85,80 +109,165 @@ class Bucket(object):
         if lon < -180:
             lon = -180
         x = int(math.floor((d_lon - lon) / width))
-        return Bucket(lon, lat, x, y)
+        return cls(lon, lat, x, y)
 
 
-def download(bucket, to):
-    out_dir = os.path.join(to, 'Orthophotos', bucket.get_base_path())
-    if not os.path.isdir(out_dir):
-        os.makedirs(out_dir)
-    full_out_path = os.path.join(out_dir, str(bucket.get_index()) + '.png')
-    print('Downloading to: ' + full_out_path)
+class ImageProvider:
+    """ Downloads a image from a URL """
 
-    if os.path.exists(full_out_path):
-        print('Target orthophoto already exists')
-        return
+    def __init__(self, url):
+        """
+        Params:
+            url: format string with the url. 'tbounds(minlon,minlat,maxlon,maxlat)' is used for tile bounds and 'tsize(width,height)' for tile size, in pixels. See examples.
+        """
+        self._url = url
 
-    bounds = bucket.get_bounds()
-    width = int(4096 * ((get_tile_width(bucket.lat)) / 0.125))
-    height = 4096
-    url = 'http://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/export' \
-          '?bbox=%f,%f,%f,%f&bboxSR=4326&size=%d,%d&imageSR=4326&format=png24&f=image' \
-          % (bounds['min_lon'], bounds['min_lat'], bounds['max_lon'], bounds['max_lat'], width, height)
-    print('URL: ' + url)
+    def download(self, bucket, outpath, tnum=(1,1), theight=512, dry_run=False):
+        """ Downloads a FG bucket and save in outpath.
+        A bucket is the final image for FG. A tile is each one of the little images that create a bucket. Many online
+        services won't allow downloading huge images at once, and you must cut buckets down into tiles.
 
-    response = requests.get(url)
+        Params:
+            bucket: a Bucket object
+            outpath: string, path to the output file
+            tnum: (cols,rows) number of tiles in the bucket. Use (1,1), (2,2), (4,4)... other pairs are not tested
+            theight: height of a tile, in pixels. The width depends on the latitude. Use power of two numbers: 512, 1028, 2048... The final image will have a theight of tsize*tnum
+            dry_run: if True, do not donwload anything. Useful for testing
+        """
 
-    if response.status_code != 200:
-        print('Failed to download orthophoto')
-        print('Status Code: %d' % response.status_code)
-        return
-    
-    if response.headers['Content-Type'] != 'image/png':
-        print('Received invalid response type. Expected "image/png", got "%s"' % response.headers['Content-Type'])
-        return
+        # final size of a tile, in pixels. Width depends on the latitude. Both sizes must be power of two
+        tsize = (int(theight * ((get_tile_width(bucket.lat)) / TILE_HEIGHT)), theight)
+        # bounds of the bucket
+        bounds = bucket.get_bounds()
+        # width of the bucket, in degrees
+        bwidth = get_tile_width(bucket.lat)
+        # size of the tile, in degrees
+        gtwidth = bwidth / tnum[0]
+        gtheight = TILE_HEIGHT / tnum[1]
+        # An array with files objects with each tile
+        ftiles = []
 
-    specified_content_length = int(response.headers['Content-Length'])
-    received_content_length = 0
+        for r in range(0, tnum[1]):
+            for c in range(0, tnum[0]):
+                min_lon = bounds['min_lon'] + c * gtwidth
+                min_lat = bounds['min_lat'] + r * gtheight
+                max_lon = min_lon + gtwidth
+                max_lat = min_lat + gtheight
+                tbounds = (min_lon, min_lat, max_lon, max_lat)
+                ftiles.append(self._download_tile(tbounds=tbounds, tsize=tsize, dry_run=dry_run))
+        if not dry_run:
+            logging.info('Joining tiles to %s', outpath)
+            with open(outpath, 'wb') as fout:
+                # note we always combine all tiles, even in the (1,1) case
+                self._join(fout, ftiles=ftiles, tnum=tnum,)
+        for f in ftiles:
+            f.close()
 
-    with open(full_out_path, 'wb') as fd:
-        for chunk in response.iter_content(chunk_size=128):
-            received_content_length += len(chunk)
-            fd.write(chunk)
+    def _download_tile(self, tbounds, tsize=(512, 256), dry_run=False):
+        """ Downloads a tile from the remote server and returns a file object.
+        A bucket is the final image for FG. A tile is each one of the little images that create a bucket. Many online
+        services won't allow downloading huge images at once, and you must cut buckets down into tiles.
 
-    if (specified_content_length != received_content_length):
-        print('WARNING: Expected %d bytes, but received %d bytes. The downloaded photo may be corrupt.' % (specified_content_length, received_content_length))
+        Params:
+            tbounds: bounds of the tile, in degrees
+            tsize: size of the tile, in pixels
+            dry_run: if True, do not donwload anything. Useful for testing
+
+        Returns:
+            The tile, as a file object.
+        """
+
+        fout = tempfile.NamedTemporaryFile(mode='w+b')
+        url = self._url.format(tbounds=tbounds, tsize=tsize)
+        logging.info('Downloading tile=%s from url=%s', fout.name, url)
+
+        if not dry_run:
+            response = requests.get(url)
+
+            if response.status_code != 200:
+                raise Exception('Failed to download orthophoto. status={}'.format(response.status_code))
+            if response.headers['Content-Type'] != 'image/png':
+                raise Exception('Received invalid response type. Expected "image/png", got content_type="{}"'.format(response.headers['Content-Type']))
+
+            for chunk in response.iter_content(chunk_size=128):
+                fout.write(chunk)
+
+        # move to the begining of the file: it must be read to combine these files into a bucket
+        fout.seek(0)
+        return fout
+
+    def _join(self, fout, ftiles, tnum=(1,1)):
+        """ Join a collection of files (tile images) into a single file.
+
+        Params:
+            fout: File object to save the final image
+            ftiles: the array of files with the files. First cols, then rows.
+            tnum: (cols,rows) in tiles for a bucket
+        """
+        try:
+            images = [Image.open(x) for x in ftiles]
+        except UnidentifiedImageError:
+            logging.error("PIL.UnidentifiedImageError: this usually means that a tile couldn't be downloaded. Exiting")
+            raise
+        width = images[0].size[0]
+        height = images[0].size[1]
+        new_im = Image.new('RGB', (width * tnum[0], height * tnum[1]))
+        for c in range(0, tnum[0]):
+            for r in range(0, tnum[1]):
+                # remember: for paste(), (0,0) is the upper left corner
+                new_im.paste(images[r * tnum[0] + c], (c * width, (tnum[1] - r - 1) * height))
+        new_im.save(fout)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download photoscenery for a tile. '
-                                                 'Provide either index OR lon and lat')
-    parser.add_argument('--index', type=int, required=False)
-    parser.add_argument('--lon', type=float, required=False)
-    parser.add_argument('--lat', type=float, required=False)
-    parser.add_argument('--info_only', dest='info_only', action='store_true', default=False)
-    parser.add_argument('--scenery_folder', type=str, required=False, default=os.getcwd())
+    parser = argparse.ArgumentParser(description="Download photoscenery for a tile. Provide either index OR lon and lat")
+    parser.add_argument('--index', type=int, required=False, help="FG tile index to download. It has preference on lat,lon")
+    parser.add_argument('--lon', type=float, required=False, help="Longitude included inside the tile to download. Ignored if an index is provided")
+    parser.add_argument('--lat', type=float, required=False, help="Latitude included inside the tile to download. Ignored if an index is provided")
+    parser.add_argument('--theight', type=int, required=False, default=2048, help='''
+        Height of a tile, in pixels. Defaults to 2048. The final image will have theight*cols pixels. Use only power of two numbers.
+        Note that most orthophoto servers will not serve orthophotos with any dimension greater than 4096.
+    ''')
+    parser.add_argument('--cols', type=int, default=1, help="Number of rows and cols for tiles in a bucket. Use only power of two numbers ")
+    parser.add_argument('--provider', default='ArcGIS', help="Name of the image provider. Currently: ArcGIS (default, covers the whole world), PNOA (Spain), or USGS (United States)")
+    parser.add_argument('--dry_run', dest='dry_run', action='store_true', default=False, help="If set, do not download anything")
+    parser.add_argument('--verbose', dest='verbose', action='store_true', default=False, help="If set, be verbose")
+    parser.add_argument('--scenery_folder', type=str, required=False, default=os.getcwd(), help="Scenery directory, for the output")
+    parser.add_argument('--overwrite', dest='overwrite', action='store_true', default=False, help='Overwrite the orthophoto if it already exists')
     args = vars(parser.parse_args())
+
+    logging.basicConfig(level=(logging.DEBUG if args['verbose'] else logging.INFO))
+
     index = args['index']
     lon = args['lon']
     lat = args['lat']
-    info_only = args['info_only']
-    to = os.path.abspath(args['scenery_folder'])
+    # create the bucket from the user input
     bucket = None
     if index is not None:
         bucket = Bucket.from_index(index)
     elif lon is not None and lat is not None:
         bucket = Bucket.from_lon_lat(lon, lat)
     else:
-        print('You gotta give me something!')
+        logging.error('You gotta give me lon, lat or index!')
         exit(1)
 
-    print('Bucket index: %d' % bucket.get_index())
-    print('Base path: %s' % bucket.get_base_path())
-    print(bucket.get_bounds())
+    logging.debug('Bucket: %s. Index: %s', bucket, bucket.get_index())
 
-    if not info_only:
-        download(bucket, to)
+
+    # create the output directory
+    dir_out_path = os.path.join(os.path.abspath(args['scenery_folder']), 'Orthophotos', bucket.get_base_path())
+
+    if not args['dry_run']:
+        os.makedirs(dir_out_path, exist_ok=True)
+    full_out_path = os.path.join(dir_out_path, str(bucket.get_index()) + '.png')
+
+    if not args['overwrite'] and os.path.exists(full_out_path):
+        logging.error('Target orthophoto already exists, skipping. Pass --overwrite to override this check.')
+        exit(1)
+
+    provider = ImageProvider(URLS[args['provider']])
+    provider.download(bucket, full_out_path, tnum=(args['cols'],args['cols']), theight=args['theight'], dry_run=args['dry_run'])
+
 
 
 if __name__ == '__main__':
