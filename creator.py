6f5832aa9a839bcbc87527bf4680e6bd4db48f4e
Nathaniel Warner <nathanielwarner77@gmail.com>
2021-01-14 11:54:04 -0800

    Add author info

--- creator.py
+++ creator.py
@@ -1,3 +1,24 @@
+#!/usr/bin/python3
+
+# Copyright (C) 2021  Nathaniel MacArthur-Warner
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#  WARNING: Only do this for providers whose TOS allow this!
+#  WARNING: This may really strain remote ressources and cost the provider
+#           real $$$, so use with care and if in doubt, ask before using!
+
 import math
 import argparse
 import requests
